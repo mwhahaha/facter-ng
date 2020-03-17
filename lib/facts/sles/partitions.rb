5e96fe0d0b5eccbb254d72a129af8bf9e35e088c
Oana Tanasoiu <oana.tanasoiu@puppet.com>
2020-03-17 12:16:46 +0200

    (FACT-2471) Add Linux partitions fact

---
+++ lib/facts/sles/partitions.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Facts
+  module Sles
+    class Partitions
+      FACT_NAME = 'partitions'
+
+      def call_the_resolver
+        partitions = extract_partitions
+
+        Facter::ResolvedFact.new(FACT_NAME, partitions)
+      end
+
+      def extract_partitions
+        parts = Facter::Resolvers::Partitions.resolve(:partitions)
+        mountpoints = Facter::Resolvers::Linux::Mountpoints.resolve(:mountpoints)
+        return parts unless mountpoints
+
+        mountpoints.each do |mnt|
+          next unless parts[mnt[:device]]
+
+          parts[mnt[:device]].merge!(mount: mnt[:path])
+        end
+        parts.empty? ? nil : parts
+      end
+    end
+  end
+end
